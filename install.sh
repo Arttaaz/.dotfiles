ec1739d89157bfa41ba821062905a560e2ca636b
Arttaaz <arttaaz@gmail.com>
2020-09-25 16:17:38 +0200

    stow in install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ if test ! -f ~/.local/share/nvim/site/autoload/plug.vim; then
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     nvim -c 'PlugInstall!'
 fi
+
+stow -t $HOME nvim i3 topgrade
