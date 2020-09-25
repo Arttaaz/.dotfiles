9bdc24cd8f343b4f906b97e05b4dfcb044bef2ec
Arttaaz <arttaaz@gmail.com>
2020-09-25 16:11:52 +0200

    PlugInstall

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+
 if test ! -f ~/.local/share/nvim/site/autoload/plug.vim; then
     curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  fi
-
+    nvim -c 'PlugInstall!'
+fi
